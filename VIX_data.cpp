e1fbcdb922aa970ae8a256c27bff9896fae31f0e
sambhavu <30390294+sambhavu@users.noreply.github.com>
2019-11-01 17:42:14 -0400

    Create VIX_data.cpp
    initialize values into algorithm for analysis

---
+++ VIX_data.cpp
@@ -0,0 +1,69 @@
+#include<algorithm>
+#include<cmath>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+
+using namespace std; 
+
+
+
+class VIX{
+    public: 
+    
+    const double pi = 3.1415926535897932384626433;
+    const int size = 7518;
+
+
+    std::vector<double> open;
+    std::vector<double> high;
+    std::vector<double> low;
+    std::vector<double> close;
+    
+    VIX();
+    void data();
+};
+
+VIX::VIX(){
+    data();
+}
+
+void VIX :: data(){
+    double value; 
+    std::ifstream infile;
+    infile.open("/users/sunitaupadhyay/Desktop/vix_open.txt", std::ifstream::binary);
+    for(int i = 0; i<size; i++)
+    {
+        infile>>value;
+        open.push_back(value);
+    }
+    
+    infile.open("/users/sunitaupadhyay/Desktop/vix_high.txt", std::ifstream::binary);
+    for(int i = 0; i<size; i++)
+    {
+        infile>>value;
+        high.push_back(value);
+    }
+    
+    infile.open("/users/sunitaupadhyay/Desktop/vix_low.txt", std::ifstream::binary);
+    for(int i = 0; i<size; i++)
+    {
+        infile>>value;
+        low.push_back(value);
+    }
+    
+    infile.open("/users/sunitaupadhyay/Desktop/vix_close.txt", std::ifstream::binary);
+    for(int i = 0; i<size; i++)
+    {
+        infile>>value;
+        close.push_back(value);
+    }
+}
+
+
+int main(){
+    
+    VIX vol; 
+    return 0;
+}
